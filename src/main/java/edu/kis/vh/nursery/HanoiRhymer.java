cf98b47a464282e75ae1468c383a94f47588ea18
Jakup27 <240799@edu.p.lodz.pl>
2023-04-18 22:07:21 +0200

    Zad 6, 7, 8

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,12 +2,14 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-    int totalRejected = 0;
+    public static final int INT1 = 0;
+    int totalRejected = INT1;
 
     public int reportRejected() {
         return totalRejected;
     }
 
+    @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
