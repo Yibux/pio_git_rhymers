faa9631e832e081b9c70b9eb5d3c40fd2e3f40d5
Jakub Szewczyk <240810@edu.p.lodz.pl>
2023-05-09 18:14:56 +0200

    17. Opisanie klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,18 @@
 package edu.kis.vh.nursery;
 
+/**
+ * klasa FIFORhymer dziedziczy po klasie DefaultCountingOutRhymer
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * Obiekt typu DefaultCountingOutRhymer o nazwie temp
+     */
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * @return int - metoda wyciaga element ze stosu i zwraca jego wartość
+     */
     @Override
     protected int countOut() {
         while (!callCheck())
